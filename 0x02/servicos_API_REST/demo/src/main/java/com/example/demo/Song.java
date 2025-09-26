8582dfb2b16b0c4e2dafca3fc3ad958644b7454b
Solange <solange_freitas01@hotmail.com>
2025-09-26 16:00:18 -0300

    First Commit

--- 0x02/servicos_API_REST/demo/src/main/java/com/example/demo/Song.java
+++ 0x02/servicos_API_REST/demo/src/main/java/com/example/demo/Song.java
@@ -1,7 +1,8 @@
 package com.example.demo;
 
-import java.util.Objects;
+import lombok.Data;
 
+@Data
 public class Song {
     private Integer id;
     private String nome;
@@ -17,69 +18,5 @@ public class Song {
         this.artista = artista;
         this.album = album;
         this.anoLancamento = anoLancamento;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getArtista() {
-        return artista;
-    }
-
-    public void setArtista(String artista) {
-        this.artista = artista;
-    }
-
-    public String getAlbum() {
-        return album;
-    }
-
-    public void setAlbum(String album) {
-        this.album = album;
-    }
-
-    public String getAnoLancamento() {
-        return anoLancamento;
-    }
-
-    public void setAnoLancamento(String anoLancamento) {
-        this.anoLancamento = anoLancamento;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Song song = (Song) o;
-        return Objects.equals(id, song.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Song{" +
-            "id=" + id +
-            ", nome='" + nome + '\'' +
-            ", artista='" + artista + '\'' +
-            ", album='" + album + '\'' +
-            ", anoLancamento='" + anoLancamento + '\'' +
-            '}';
     }
 }
