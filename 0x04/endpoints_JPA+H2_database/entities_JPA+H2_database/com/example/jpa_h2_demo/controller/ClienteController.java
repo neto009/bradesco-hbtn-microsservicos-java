5535134a09325b518e666544d17e52050c5ca9e2
Solange <solange_freitas01@hotmail.com>
2025-09-26 22:50:51 -0300

    First Commit

---
+++ 0x04/endpoints_JPA+H2_database/entities_JPA+H2_database/com/example/jpa_h2_demo/controller/ClienteController.java
@@ -0,0 +1,51 @@
+package com.example.jpa_h2_demo.controller;
+
+import com.example.jpa_h2_demo.model.Cliente;
+import com.example.jpa_h2_demo.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @PostMapping("/addClient")
+    public ResponseEntity<Cliente> addClient(@RequestBody Cliente cliente) {
+        Cliente novoCliente = clienteRepository.save(cliente);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoCliente);
+    }
+
+    @GetMapping("/findAllClients")
+    public ResponseEntity<List<Cliente>> findAllClients() {
+        List<Cliente> clientes = clienteRepository.findAll();
+        return ResponseEntity.ok(clientes);
+    }
+
+    @GetMapping("/findClientById/{id}")
+    public ResponseEntity<Cliente> findClientById(@PathVariable("id") Long idCliente) {
+        Optional<Cliente> cliente = clienteRepository.findById(idCliente);
+        return cliente.map(ResponseEntity::ok)
+            .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/removeClientById/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removerCliente(@PathVariable("id") Long idCliente) {
+        clienteRepository.deleteById(idCliente);
+    }
+
+    @PutMapping("/updateClientById/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateCliente(@PathVariable("id") Long id, @RequestBody Cliente cliente) {
+        cliente.setId(id);
+        clienteRepository.save(cliente);
+    }
+}
