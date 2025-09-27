7c39cf63f5fd4f2b8ff5da1e9b1006f087fd51c0
Solange <solange_freitas01@hotmail.com>
2025-09-26 22:15:51 -0300

    First Commit

---
+++ 0x04/entities_JPA+H2_database/com/example/jpa_h2_demo/model/Telefone.java
@@ -0,0 +1,30 @@
+package com.example.jpa_h2_demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Telefone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String ddd;
+    private String numero;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getDdd() { return ddd; }
+    public void setDdd(String ddd) { this.ddd = ddd; }
+
+    public String getNumero() { return numero; }
+    public void setNumero(String numero) { this.numero = numero; }
+
+    public Cliente getCliente() { return cliente; }
+    public void setCliente(Cliente cliente) { this.cliente = cliente; }
+}
